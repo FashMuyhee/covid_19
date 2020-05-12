73cc1f8d68150ba727a202c7eb9af5bac890ba8e
fashmuyhee <fashmuyidavid@gmail.com>
2020-05-12 12:56:54 +0100

    added new screen

--- src/route/navigator.js
+++ src/route/navigator.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {HomeScreen} from '../screens';
+import {HomeScreen, LandingScreen} from '../screens';
 
 const Stack = createStackNavigator();
 
 export const AppNavigator = () => (
   <NavigationContainer>
-    <Stack.Navigator headerMode="none" initialRouteName="home">
+    <Stack.Navigator headerMode="none" initialRouteName="landing">
       <Stack.Screen name="home" component={HomeScreen} />
+      <Stack.Screen name="landing" component={LandingScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
