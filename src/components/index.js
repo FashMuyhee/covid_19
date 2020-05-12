9467c28a4f739432f211a2c6b709c25b33df87c0
fashmuyhee <fashmuyidavid@gmail.com>
2020-05-12 19:59:31 +0100

    imported scrollcontianer and box component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Container from './Container';
 import Navbar from './Navbar';
+import ScrollContainer from './ScrollContainer';
+import Box from './Box';
 
-export {Container, Navbar};
+export {Container, Navbar, ScrollContainer,Box};
