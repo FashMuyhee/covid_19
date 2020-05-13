7cc5d86a4a8c41d2a267e8ad60d8624b68ff8f09
fashmuyhee <fashmuyidavid@gmail.com>
2020-05-13 02:52:59 +0100

    added new screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,4 +1,6 @@
 import HomeScreen from './Home';
 import LandingScreen from './Landing';
+import PreventionScreen from './Prevention';
+import SymptomsScreen from './Symptoms';
 
-export {HomeScreen, LandingScreen};
+export {HomeScreen, LandingScreen, PreventionScreen, SymptomsScreen};
