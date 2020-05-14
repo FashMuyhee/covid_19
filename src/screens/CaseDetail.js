722e5c9f69ad9128b6960b858c390261f7ada21c
fashmuyhee <fashmuyidavid@gmail.com>
2020-05-14 23:58:36 +0100

    initilal commit

---
+++ src/screens/CaseDetail.js
@@ -0,0 +1,103 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {
+  Text,
+  Layout,
+  StyleService,
+  useStyleSheet,
+  useTheme,
+  Icon,
+  Radio,
+  Button,
+} from '@ui-kitten/components';
+import {ScrollContainer, Box} from '../components';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+
+const CaseDetailScreen = () => {
+  const currentDate = new Date().toDateString();
+  const styles = useStyleSheet(themedStyles);
+  const theme = useTheme();
+  const [state] = useState({
+    location: 'nigeria',
+    cases: [],
+  });
+  const {location, cases} = state;
+
+  const locationIcon = (props) => {
+    return <Icon {...props} name="pin" />;
+  };
+
+  return (
+    <ScrollContainer style={styles.container}>
+      <Layout style={styles.headerText}>
+        <Text category="h5">Covid-19</Text>
+        <Text category="h5">Nigeria : Case Detail</Text>
+        <Text appearance="hint">Newest update {currentDate}</Text>
+      </Layout>
+
+      <Box customStyle={styles.case}>
+        <Box customStyle={styles.caseBox}>
+          <Radio checked={true} status="warning" />
+          <Text style={styles.boxText} category="h3" status="warning">
+            693494
+          </Text>
+          <Text style={styles.boxText}>Total Case</Text>
+        </Box>
+        <Box customStyle={styles.caseBox}>
+          <Radio checked={true} status="success" />
+          <Text style={styles.boxText} category="h3" status="success">
+            6700
+          </Text>
+          <Text style={styles.boxText}>Recovered Case</Text>
+        </Box>
+        <Box customStyle={styles.caseBox}>
+          <Radio checked={true} status="danger" />
+          <Text style={styles.boxText} category="h3" status="danger">
+            12
+          </Text>
+          <Text style={styles.boxText}>Death Case</Text>
+        </Box>
+      </Box>
+    </ScrollContainer>
+  );
+};
+
+export default CaseDetailScreen;
+
+const themedStyles = StyleService.create({
+  container: {},
+  headerText: {
+    height: hp('15%'),
+    marginTop: hp('6%'),
+  },
+  case: {
+    // backgroundColor: 'color-primary-200',
+    borderColor: 'color-primary-200',
+    marginBottom: hp(5),
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingLeft: '0%',
+    paddingRight: '0%',
+    paddingTop: '0%',
+    paddingBottom: '0%',
+    // borderWidth: 0,
+  },
+  caseBox: {
+    backgroundColor: 'transparent',
+    borderWidth: 0,
+    width: wp(26),
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingLeft: '0%',
+    paddingRight: '0%',
+    paddingTop: '0%',
+    paddingBottom: '0%',
+  },
+  boxText: {
+    textAlign: 'center',
+  },
+});
---
+++ src/screens/Cases.js
@@ -0,0 +1,212 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {
+  Text,
+  Layout,
+  Autocomplete,
+  AutocompleteItem,
+  StyleService,
+  useStyleSheet,
+  useTheme,
+  Icon,
+  Radio,
+  Button,
+} from '@ui-kitten/components';
+import {ScrollContainer, Box} from '../components';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import {
+  Image,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+} from 'react-native';
+
+const CasesScreen = ({navigation}) => {
+  const currentDate = new Date().toDateString();
+  //   const currentDate = `${date[1]} ${date[2]}`;
+  const styles = useStyleSheet(themedStyles);
+  const theme = useTheme();
+  const [state, setState] = useState({
+    location: 'nigeria',
+    cases: [],
+    countries: [
+      {title: 'Star Wars'},
+      {title: 'Back to the Future'},
+      {title: 'The Matrix'},
+      {title: 'Inception'},
+      {title: 'Interstellar'},
+    ],
+  });
+  const {countries, location, cases} = state;
+  const filter = (item, query) =>
+    item.title.toLowerCase().includes(query.toLowerCase());
+
+  const locationChange = (value) => {
+    setState({
+      ...state,
+      location: value,
+      countries: countries.filter((item) => filter(item, value)),
+    });
+  };
+  const onSelect = (index) => {
+    setState({...state, location: countries[index].title});
+  };
+
+  const clearInput = () => {
+    setState({...state, location: ' '});
+  };
+
+  const locationIcon = (props) => {
+    return <Icon {...props} name="pin" />;
+  };
+
+  const clearIcon = (props) => (
+    <TouchableWithoutFeedback onPress={clearInput}>
+      <Icon {...props} name="close" />
+    </TouchableWithoutFeedback>
+  );
+
+  const renderOption = (item, index) => (
+    <AutocompleteItem
+      key={index}
+      title={item.title}
+      accessoryLeft={locationIcon}
+    />
+  );
+  return (
+    <ScrollContainer style={styles.container}>
+      <Layout style={styles.headerText}>
+        <Text category="h5">Covid-19</Text>
+        <Text category="h3">Case Update</Text>
+      </Layout>
+      <Box customStyle={styles.card}>
+        <Layout style={{backgroundColor: 'transparent', width: wp('50%')}}>
+          <Text category="h4" style={{...styles.text, textAlign: 'left'}}>
+            What you need is to stay at home.
+          </Text>
+        </Layout>
+        <Image
+          source={require('../assets/img/doc.png')}
+          style={{
+            height: hp('30%'),
+            width: wp('30%'),
+            marginBottom: hp('5.6%'),
+          }}
+        />
+      </Box>
+
+      {/* <KeyboardAvoidingView> */}
+      <Layout style={styles.content}>
+        <Autocomplete
+          value={location}
+          placeholder="Enter Location"
+          accessoryLeft={locationIcon}
+          accessoryRight={clearIcon}
+          onChangeText={(value) => locationChange(value)}
+          onSelect={onSelect}
+          style={styles.input}
+          textStyle={{textTransform: 'capitalize'}}>
+          {countries.map(renderOption)}
+        </Autocomplete>
+        <Layout style={{marginTop: 10}}>
+          <Layout style={styles.caseLink}>
+            <Layout>
+              <Text category="h5">Case Update</Text>
+              <Text appearance="hint">Newest update {currentDate}</Text>
+            </Layout>
+            <Button
+              appearance="ghost"
+              onPress={() => navigation.navigate('case_detail')}>
+              <Text status="info"> See Details</Text>
+            </Button>
+          </Layout>
+          <Box customStyle={styles.case}>
+            <Box customStyle={styles.caseBox}>
+              <Radio checked={true} status="warning" />
+              <Text style={styles.boxText} category="h3" status="warning">
+                9349
+              </Text>
+              <Text style={styles.boxText}>Infected</Text>
+            </Box>
+            <Box customStyle={styles.caseBox}>
+              <Radio checked={true} status="success" />
+              <Text style={styles.boxText} category="h3" status="success">
+                6700
+              </Text>
+              <Text style={styles.boxText}>Recovered</Text>
+            </Box>
+            <Box customStyle={styles.caseBox}>
+              <Radio checked={true} status="danger" />
+              <Text style={styles.boxText} category="h3" status="danger">
+                12
+              </Text>
+              <Text style={styles.boxText}>Deaths</Text>
+            </Box>
+          </Box>
+        </Layout>
+      </Layout>
+      {/* </KeyboardAvoidingView> */}
+    </ScrollContainer>
+  );
+};
+
+export default CasesScreen;
+
+const themedStyles = StyleService.create({
+  container: {},
+  headerText: {
+    height: hp('15%'),
+    marginTop: hp('6%'),
+  },
+  card: {
+    backgroundColor: 'color-primary-600',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyItems: 'space-between',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'color-basic-100',
+    textTransform: 'capitalize',
+    textAlign: 'center',
+  },
+  content: {
+    marginTop: hp(3),
+  },
+  input: {
+    borderRadius: 25,
+    borderColor: 'color-info-300',
+    backgroundColor: 'color-primary-100',
+  },
+  caseLink: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  case: {
+    // backgroundColor: 'color-primary-200',
+    borderColor: 'color-primary-200',
+    marginTop: hp(4),
+    marginBottom: hp(5),
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingLeft: '0%',
+    paddingRight: '0%',
+    paddingTop: '0%',
+    paddingBottom: '0%',
+    // borderWidth: 0,
+  },
+  caseBox: {
+    backgroundColor: 'transparent',
+    borderWidth: 0,
+    width: wp(26),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  boxText: {
+    textAlign: 'center',
+  },
+});
