8affc07d2c207ced248adbbed7d748f448f9f723
fashmuyhee <fashmuyidavid@gmail.com>
2020-05-19 03:25:01 +0100

    configured mobx

--- index.js
+++ index.js
@@ -5,5 +5,16 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react';
+import {Provider} from 'mobx-react';
+import Store from './src/store/store';
 
-AppRegistry.registerComponent(appName, () => App);
+const MainApp = () => {
+  return (
+    <Provider store={Store}>
+      <App />
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => MainApp);
